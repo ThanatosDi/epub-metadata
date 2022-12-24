ad1379218cdc5ba29c5b2285b75264355cf0be52
ThanatosDi <yykkold55tw@gmail.com>
2022-12-24 17:21:58 +0800

    release: 1.0.1b0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="epub-metadata",
-    version="1.0.0b0",
+    version="1.0.1b0",
     author="ThanatosDi",
     author_email="ThanatosDi@kttsite.com",
     description="https://github.com/ThanatosDi/epub-metadata",
